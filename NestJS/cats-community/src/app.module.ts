d38c9147fdc81abd0d0401d71ea38a0e49b2701f
jmj0k <mindco03@naver.com>
2022-08-18 01:51:19 +0900

    mongodb conn

--- NestJS/cats-community/src/app.module.ts
+++ NestJS/cats-community/src/app.module.ts
@@ -1,10 +1,20 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
 import { LoggerMiddleware } from './common/middlewares/logger.middleware';
 @Module({
-  imports: [CatsModule],
+  imports: [
+    // 환경변수 사용하려면 반드시 configModule을 import 해야함
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGODB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }),
+    CatsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
